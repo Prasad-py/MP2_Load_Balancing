a73d06915b796b84917a81574681ee99d73e969b
Prasad Gole <62655027+Prasad-py@users.noreply.github.com>
2024-01-26 12:32:46 +0530

    Update serve.py

--- serve.py
+++ serve.py
@@ -16,4 +16,4 @@ def stress():
     return jsonify({"message": "Stressing CPU now"}), 202
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
